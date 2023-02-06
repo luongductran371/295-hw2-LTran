df36938bc8bfa2bd7d882cd84527a0f1b7b79f0c
Luong Tran <luong.tran@hope.edu>
2023-02-06 11:13:18 -0500

    contributions

---
+++ contributions.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+my @logs = `git log --pretty=format:"%ae,%ct,%s"`;
+
+my %contributions;
+
+foreach my $log (@logs) {
+    my ($email, $timestamp, $commit_message) = split(',', $log);
+    if (exists $contributions{$email}) {
+        $contributions{$email}{'count'}++;
+        $contributions{$email}{'latest_commit'} = $timestamp if $timestamp > $contributions{$email}{'latest_commit'};
+    } else {
+        $contributions{$email} = {
+            'count' => 1,
+            'first_commit' => $timestamp,
+            'latest_commit' => $timestamp,
+        };
+    }
+}
+
+foreach my $email (sort keys %contributions) {
+    my $count = $contributions{$email}{'count'};
+    my $first_commit = $contributions{$email}{'first_commit'};
+    my $latest_commit = $contributions{$email}{'latest_commit'};
+    print "$email , $count , " . localtime($first_commit) . " , " . localtime($latest_commit) . "\n";
+}
