976c6145f9c9dd4b116e27668cf20ca18fa5dd46
Luong Tran <luong.tran@hope.edu>
2023-03-09 15:34:07 -0500

    Tree_sha.pl revised

--- tree_sha.pl
+++ tree_sha.pl
@@ -3,9 +3,9 @@ use warnings;
 
 my $commit_sha = $ARGV[0];
 
-my $tree_sha = `git rev-parse $commit_sha^{tree}`;
-chomp $tree_sha;
+my @commit_content = `git cat-file -p $commit_sha`;
+
+my @tree = split(' ',$commit_content[0]);
+my $tree_sha = $tree[1];
 
 print "$tree_sha\n";
-
-hi;
