205291c4a74e6fdfb8af89129ef95d48b6fe056d
Luong Tran <luong.tran@hope.edu>
2023-03-10 11:59:29 -0500

    file_contents_revised completed

--- file_contents.pl
+++ file_contents.pl
@@ -2,27 +2,57 @@
 use strict;
 use warnings;
 
-my $file = shift;
+my $file_name = shift;
 
-if (! -e $file) {
+if (! -e $file_name) {
     print "Error: file does not exist\n";
     exit;
 }
 
-my $sha = `git log --pretty=format:'%H' -n 1 $file`;
-chomp $sha;
+open(my $fh, '<', '.git/HEAD') or die "Could not open file: $!";
+my $content = do { local $/; <$fh> };
+close($fh);
+my @head_ref_array = split(' ', $content);
+$content = $head_ref_array[1];
 
-my $author = `git log --pretty=format:'%an' -n 1 $file`;
-chomp $author;
+open($fh, '<', ".git/$content") or die "Could not open file: $!";
+$content = do { local $/; <$fh> };
+close($fh);
 
-my $author_email = `git log --pretty=format:'%ae' -n 1 $file`;
-chomp $author_email;
+my $commit_content = `git cat-file -p $content`;
 
+if ($commit_content =~ /^author\s+(.+)\s+<(.+)>/m) {
+    my $author_name = $1;
+    my $author_email = $2;
+    $author_name =~ s/^\s+//;  # Remove leading whitespace
+    $author_name =~ s/\s+$//;  # Remove trailing whitespace
+    print "Author: $author_name $author_email\n\n";
+} else {
+    print "Could not extract author's name and email from commit content.\n";
+}
 
-print "Author: $author $author_email\n\n";
-print "SHA-1 for $file: $sha\n\n";
+my @commit_content = `git cat-file -p $content`;
 
-open(my $fh, "<", $file) or die "Error opening file: $!";
+my @tree = split(' ',$commit_content[0]);
+my $tree_sha = $tree[1];
+my $tree_content = `git cat-file -p $tree_sha`;
+
+my $file_sha1;
+foreach my $line (split /\n/, $tree_content) {
+    my ($mode, $type, $sha1, $name) = split /\s+/, $line;
+    if ($name eq $file_name) {
+        $file_sha1 = $sha1;
+        last;
+    }
+}
+
+if (defined $file_sha1) {
+    print "SHA-1 for $file_name: $file_sha1\n\n";
+} else {
+    print "File not found in tree.\n";
+}
+
+open($fh, "<", $file_name) or die "Error opening file: $!";
 while (my $line = <$fh>) {
     print $line;
 }
