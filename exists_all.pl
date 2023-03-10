b228929f773a41d9a38e0b3363231e57b2356bdc
Luong Tran <luong.tran@hope.edu>
2023-03-10 12:41:59 -0500

    Exists_all_revised completed

--- exists_all.pl
+++ exists_all.pl
@@ -5,7 +5,7 @@ if (@ARGV < 2) {
   die "Usage: $0 <filename> <commit 1> <commit 2> ...\n";
 }
 
-my $filename = shift @ARGV;
+my $file_name = shift @ARGV;
 my @commits = @ARGV;
 
 foreach my $commit (@commits) {
@@ -19,11 +19,31 @@ foreach my $commit (@commits) {
     exit 1;
   }
 
-  $output = `git ls-tree $commit $filename`;
-  if (!$output) {
+  my @commit_content = `git cat-file -p $commit`;
+
+  my @tree = split(' ',$commit_content[0]);
+  my $tree_sha = $tree[1];
+  my $tree_content = `git cat-file -p $tree_sha`;
+
+  my $exists = 0;
+
+  foreach my $line (split /\n/, $tree_content) {
+    my ($mode, $type, $sha1, $name) = split /\s+/, $line;
+    if ($name eq $file_name) {
+        $exists = 1;
+    }
+  }
+
+  if($exists == 0){
     print "False\n";
     exit 0;
   }
+
+  # $output = `git ls-tree $commit $filename`;  
+  # if (!$output) {
+  #   print "False\n";
+  #   exit 0;
+  # }
 }
 
 print "True\n";
